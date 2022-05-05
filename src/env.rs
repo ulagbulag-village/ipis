0974311064586f4817c7fffc02bd7fa87faac3ef
Ho Kim <ho.kim@ulagbulag.io>
2022-05-05 16:05:08 +0000

    Fix infer error type

--- src/env.rs
+++ src/env.rs
@@ -1,9 +1,9 @@
-use ipi::anyhow::{anyhow, Result};
+use ipi::anyhow::{anyhow, Error, Result};
 
 pub fn infer<K: AsRef<str>, R>(key: K) -> Result<R>
 where
     R: ::core::str::FromStr,
-    <R as ::core::str::FromStr>::Err: ::std::error::Error + Send + Sync + 'static,
+    <R as ::core::str::FromStr>::Err: Into<Error> + Send + Sync + 'static,
 {
     let key = key.as_ref();
 
