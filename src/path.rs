92bde77fd8849af08c65c3119259819bbe639d53
Ho Kim <ho.kim@ulagbulag.io>
2022-05-13 01:39:58 +0000

    Add DynPath

--- src/path.rs
+++ src/path.rs
@@ -10,3 +10,12 @@ pub struct Path {
     pub value: Hash,
     pub len: u64,
 }
+
+#[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Archive, Serialize, Deserialize)]
+#[archive(compare(PartialEq))]
+#[archive_attr(derive(CheckBytes, Debug, PartialEq, Eq, PartialOrd, Ord, Hash))]
+
+pub enum DynPath {
+    Path(Path),
+    Name(String),
+}
